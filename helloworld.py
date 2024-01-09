8ed1fbc09e8e6936c354267a40c64868ac8f1a62
alinatroncoso17 <156133526+alinatroncoso17@users.noreply.github.com>
2024-01-09 12:51:29 -0800

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,16 @@
+# This is a comment
+# This program will write Hello World!
+
+#This function tells the computer to print Hello World
+def PrintHelloWorld():
+	print("Hello World!") 
+
+
+# This is the main progtam that will be
+# run by Python
+def main():
+	PrintHelloWorld()
+
+# Executes main program when script is run
+if __name__ =="__main__"
+    main()
