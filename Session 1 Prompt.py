3334df54d982708d165d977a6e99df0156585f8e
alinatroncoso17 <156133526+alinatroncoso17@users.noreply.github.com>
2024-01-22 22:12:31 -0800

    Add files via upload

---
+++ Session 1 Prompt.py
@@ -0,0 +1,12 @@
+Python 3.12.1 (v3.12.1:2305ca5144, Dec  7 2023, 17:23:39) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("Alina Troncoso")
+Alina Troncoso
+>>> print("My favorite movie is Hereditary directed by Ari Aster")
+My favorite movie is Hereditary directed by Ari Aster
+>>> print(My favorite food is french fries")
+...       
+SyntaxError: unterminated string literal (detected at line 1)
+>>> print("My favorite food is french fries")
+...       
+My favorite food is french fries
