e65658d8a3e60149b31b96055646c15857180817
alinatroncoso17 <156133526+alinatroncoso17@users.noreply.github.com>
2024-01-22 22:12:14 -0800

    Add files via upload

---
+++ Session 2 Prompt.py
@@ -0,0 +1,16 @@
+Python 3.12.1 (v3.12.1:2305ca5144, Dec  7 2023, 17:23:39) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 0.5 + 0.8
+1.3
+>>> 4 - 1
+3
+>>> 2.5 * 2.5
+6.25
+>>> print("0.5 + 0.8 = 1.3")
+0.5 + 0.8 = 1.3
+>>> int(1+3)
+4
+>>> float(0.5+0.8)
+1.3
+>>> float(2.5 * 0.8)
+2.0
