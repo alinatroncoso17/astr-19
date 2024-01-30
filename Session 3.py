ee48f52a6d0608ae7a4b51e9b75a9987b84a6176
alinatroncoso17 <156133526+alinatroncoso17@users.noreply.github.com>
2024-01-29 20:32:29 -0800

    Add files via upload

---
+++ Session 3.py
@@ -0,0 +1,217 @@
+Last login: Fri Jan 26 15:48:24 on ttys000
+
+The default interactive shell is now zsh.
+To update your account to use zsh, please run `chsh -s /bin/zsh`.
+For more details, please visit https://support.apple.com/kb/HT208050.
+Alinas-MacBook-Air:~ alinatroncoso$ session_3.py
+-bash: session_3.py: command not found
+Alinas-MacBook-Air:~ alinatroncoso$ python session_3.py
+-bash: python: command not found
+Alinas-MacBook-Air:~ alinatroncoso$ python3 session_3.py
+/Library/Frameworks/Python.framework/Versions/3.12/Resources/Python.app/Contents/MacOS/Python: can't open file '/Users/alinatroncoso/session_3.py': [Errno 2] No such file or directory
+Alinas-MacBook-Air:~ alinatroncoso$ python3 officehours.pu
+/Library/Frameworks/Python.framework/Versions/3.12/Resources/Python.app/Contents/MacOS/Python: can't open file '/Users/alinatroncoso/officehours.pu': [Errno 2] No such file or directory
+Alinas-MacBook-Air:~ alinatroncoso$ python3 officehours.py
+/Library/Frameworks/Python.framework/Versions/3.12/Resources/Python.app/Contents/MacOS/Python: can't open file '/Users/alinatroncoso/officehours.py': [Errno 2] No such file or directory
+Alinas-MacBook-Air:~ alinatroncoso$ pwd
+/Users/alinatroncoso
+Alinas-MacBook-Air:~ alinatroncoso$ ls
+Applications
+Creative Cloud Files
+Creative Cloud Files alina.troncoso.3419@mail.linnbenton.edu 49df537bb91659ea5f6e027bcb9e0601f709dd28f0f6e162ca076f6a8944d406
+Desktop
+Documents
+Downloads
+Library
+Movies
+Music
+Pictures
+Public
+SimUText 2020-2021
+Temeke Hospital.html
+output
+Alinas-MacBook-Air:~ alinatroncoso$ cd documents
+Alinas-MacBook-Air:documents alinatroncoso$ pwd
+/Users/alinatroncoso/documents
+Alinas-MacBook-Air:documents alinatroncoso$ ls
+2019-2020DisbursementAgreement-Linn-Benton copy.pdf
+ANTH101 - Research Teaching Project.key
+ANTH101 - Research Teaching Project.pptx
+Adobe
+Adobe After Effects Auto-Save
+AlinaTroncoso_WriteUp1.doc.HEIC
+Animate.fla
+Cat Colors.pages
+Cell Structures (1).pdf
+ComparingLinearandExponentialGrowth.pages
+ComparingLinearandExponentialGrowth.pdf
+Discord Introduction.pdf
+Entire Informative Speech Outline.pages
+Exam 1 Fall 2020.pages
+Exam 1 Fall 2020.pdf
+GS 108 Final Exam.pages
+GS 108 Final Exam.pdf
+GS 108 Midterm_online2.pages
+GS 108 Midterm_online2.pdf
+GS108_Lab 1_final.pages
+GS108_Lab 1_final.pdf
+GS108_Lab 2_final.pages
+GS108_Lab 2_final.pdf
+GS108_Lab 3_final (1).pages
+GS108_Lab 3_final (1).pdf
+GS108_Lab 4_final (1).pages
+GS108_Lab 4_final (1).pdf
+GS108_Lab 5_final.pages
+GS108_Lab 5_final.pdf
+GS108_Lab 6_final.pages
+GS108_Lab 6_final.pdf
+GS108_Lab 7_final.pages
+GS108_Lab 7_final.pdf
+GS108_Lab 8_final.pages
+GS108_Lab 9_final (1).pages
+GS108_Lab 9_final (1).pdf
+Graphic Portrait Animation 1.0.fla
+Graphic-Portrait-Animation-1.gif
+Health Lifestyle Change.numbers
+Human Evolution.pages
+Human Evolution.pdf
+Informative Speech Self Evaluation.pages
+Informative Speech Self Evaluation.pdf
+Introduction Speech Outline Template.pages
+Iris Vision Design.pdf
+JLTSPHTATC1 copy.aep
+JLTSPHTATC1.aep
+JLTSPHTATC1_AME
+Lab 1.pages
+Lab 1.pdf
+Lab 10.pages
+Lab 10.pdf
+Lab 2.pages
+Lab 2.pdf
+Lab 3 (3).pages
+Lab 3 (3).pdf
+Lab 4 (1).pages
+Lab 4 (1).pdf
+Lab 5 PDF.pdf
+Lab 5 online.pdf
+Lab 5.pages
+Lab 5.pdf
+Lab 7.pages
+Lab 7.pdf
+Lab 8.pages
+Library
+LogisticGrowthPractice.pdf
+Looping-Gif-1---Ball-Pop.gif
+MTH 111 - BACK.pdf
+MTH 111 - Test 1 Notecard (Back).pdf
+MTH 111 - Week 2 Written Activity.pdf
+MTH 111 - Week 3 Written Activity.pdf
+MTH 111 - Written Work + Notecard.pdf
+MTH 111 Test 1 Notecard (Front).pdf
+MTH 111.pdf
+MTH111 - FRONT.pdf
+MTH111-Back of Notecard.pdf
+MTH111-Page and Front of Notecard.pdf
+MTH111-Week 5 Written Activity.pdf
+MTH111week8activity.pdf
+Marine Pollution (1).pages
+Marine Pollution (1).pdf
+Persuasive Speech Self Evaluation.pdf
+Population Genetics.pages
+Population Genetics.pdf
+Pre-Lab 6 Protists  Fungi.pages
+Pre-Lab 6 Protists  Fungi.pdf
+Prelab 4 Nutrient Pollution.pages
+Prelab 4 Nutrient Pollution.pdf
+RENTAL APPLICATION.pages
+Rat Island.pages
+Rat Island.pdf
+Review 8.pages
+Seasons Loop 1.mov
+Session 1 Prompt.py
+Session 2 Prompt.py
+TImothys Masterpiece.mp4
+TRONCOSO_GROUPC_HW.ai
+Unofficial Transcript (Pg. 2).pdf
+Unofficial Transcript.pdf
+Week 7 Written Activity.pdf
+Winning Business Collateral.ai
+Zoom
+academic planning.htm
+academic planning_files
+cell respiration and photosynthesis.pdf
+cerealbox.pdf
+foodweb?heic.pdf
+gif2.aep
+gif2.aep Logs
+gif2_AME
+line-graph.pdf
+notecard.pdf
+officehours.py
+paris abducting helen.pdf
+test 1.pdf
+Alinas-MacBook-Air:documents alinatroncoso$ cd ..
+Alinas-MacBook-Air:~ alinatroncoso$ pwd
+/Users/alinatroncoso
+Alinas-MacBook-Air:~ alinatroncoso$ cd documents
+Alinas-MacBook-Air:documents alinatroncoso$ python3 officehours.py
+HelloWorld!
+Alinas-MacBook-Air:documents alinatroncoso$ python3 session_3.py
+/Library/Frameworks/Python.framework/Versions/3.12/Resources/Python.app/Contents/MacOS/Python: can't open file '/Users/alinatroncoso/Documents/session_3.py': [Errno 2] No such file or directory
+Alinas-MacBook-Air:documents alinatroncoso$ python3 session_3.py
+  File "/Users/alinatroncoso/Documents/session_3.py", line 8
+    print(f"The result of f({x_value})") is: {result})
+                                                     ^
+SyntaxError: unmatched ')'
+Alinas-MacBook-Air:documents alinatroncoso$ python3 session_3.py
+  File "/Users/alinatroncoso/Documents/session_3.py", line 8
+    print(f"The result of f({x_value})") is: {result})
+                                                     ^
+SyntaxError: unmatched ')'
+Alinas-MacBook-Air:documents alinatroncoso$ python3 session_3.py
+  File "/Users/alinatroncoso/Documents/session_3.py", line 7
+    result f(x_value)
+           ^
+SyntaxError: invalid syntax
+Alinas-MacBook-Air:documents alinatroncoso$ python3 session_3.py
+  File "/Users/alinatroncoso/Documents/session_3.py", line 7
+    result f(x_value)
+           ^
+SyntaxError: invalid syntax
+Alinas-MacBook-Air:documents alinatroncoso$ python3 session_3.py
+  File "/Users/alinatroncoso/Documents/session_3.py", line 11
+    if result > 27
+                  ^
+SyntaxError: expected ':'
+Alinas-MacBook-Air:documents alinatroncoso$ python3 session_3.py
+  File "/Users/alinatroncoso/Documents/session_3.py", line 11
+    if result > 27
+                  ^
+SyntaxError: expected ':'
+Alinas-MacBook-Air:documents alinatroncoso$ python3 session_3.py
+The result of f(9) is: 737
+YAY!
+Alinas-MacBook-Air:documents alinatroncoso$ python3 session_3.py
+The result of f(9) is: 737
+YAY!
+Alinas-MacBook-Air:documents alinatroncoso$ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
