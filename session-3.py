3f5a2a3f20340acc970174076c28c8352aa268c1
alinatroncoso17 <156133526+alinatroncoso17@users.noreply.github.com>
2024-01-29 20:33:33 -0800

    Add files via upload

---
+++ session-3.py
@@ -0,0 +1,12 @@
+#This program will define a function f(x) and print the result 
+#that returns x**3 + 8
+def f(x):
+	return x**3 + 8 
+def main():
+x_value = 9
+result f(x_value)
+print(f"The result of f({x_value})") is: {result})
+
+
+if result > 27
+	print("YAY!")
