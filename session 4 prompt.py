3cbad459121835b9dad6926786d6690b318f160d
alinatroncoso17 <156133526+alinatroncoso17@users.noreply.github.com>
2024-02-02 10:58:31 -0800

    Add files via upload

---
+++ session 4 prompt.py
@@ -0,0 +1,17 @@
+#This program will create a class describing my favorite animal
+#I will use "if" and "else" parameters to 
+class Deer:
+	def __init__(self, leg_length, num_eyes, has_tail, is_furry):
+		self.leg_length = leg_length
+		self.num_eyes = num_eyes
+		self.has_tail = has_tail
+		self.is_furry = is_furry
+
+	def describe_deer(self):
+		print("Deers Physical Characteristics")
+		print(f"Leg Length: {self:leg_legnth}" 81cm)
+		print("Number of Eyes {self:num_eyes}" Two Eyes)
+		print(f"Has a Tail {self:has_tail}")
+		print(f"Is Furry {self:is_furry}")
+
+		print()
